5c796565ad7e045f4065f430f4f04cab4cfcac81
Jóhann Berentsson <johann@berentsson.net>
2025-11-13 18:57:37 +0000

    CMake generated file removed.

--- source/shared/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Jóhann Berentsson/Documents/Max 8/Packages/Max-CMOS/source/shared
-# Build directory: C:/Users/Jóhann Berentsson/Documents/Max 8/Packages/Max-CMOS/source/shared
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
