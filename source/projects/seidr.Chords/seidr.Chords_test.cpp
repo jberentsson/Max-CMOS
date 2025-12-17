5ea0dff0f365d703e486e6ce7c22a8bd23858e30
Jóhann Berentsson <johann@berentsson.net>
2025-12-17 00:23:45 +0000

    Chords (#29)
    * parameters moved to a class
    * note off bug fixed

--- source/projects/seidr.Chords/seidr.Chords_test.cpp
+++ source/projects/seidr.Chords/seidr.Chords_test.cpp
@@ -93,149 +93,9 @@ SCENARIO("ChordsMax object produces correct output") { // NOLINT
             REQUIRE(velocity_output[i][1] == expectedOutput[i][1]);
         }
 
-        //REQUIRE(velocity_output.size() == 18);
-        //REQUIRE(note_output.size() == 18);
+        REQUIRE(velocity_output.size() == 18);
+        REQUIRE(note_output.size() == 18);
     }
-}
-
-SCENARIO("ChordsMax object produces correct output second try") { // NOLINT
-    ext_main(nullptr);
-
-    min::test_wrapper<ChordsMax> an_instance;
-    ChordsMax &chordsTest = an_instance;
-
-    REQUIRE_NOTHROW(chordsTest.noteMode(ChordsMax::NoteMode::RETRIGGER, 1));
-    REQUIRE(chordsTest.getNoteMode() == ChordsMax::NoteMode::RETRIGGER);
-
-    auto &note_output = *max::object_getoutput(chordsTest, 0);
-    auto &velocity_output = *max::object_getoutput(chordsTest, 1);
-
-    GIVEN("An instance of our object") {
-        REQUIRE(note_output.empty());
-        REQUIRE(velocity_output.empty());
-
-        REQUIRE_NOTHROW(chordsTest.setKey({ NoteC4, NoteC4, NoteE4, NoteG4 }, 1));  // C chord
-        REQUIRE_NOTHROW(chordsTest.setKey({ NoteD4, NoteC4, NoteF4, NoteA4 }, 1));  // F chord
-        REQUIRE_NOTHROW(chordsTest.setKey({ NoteE4, NoteC4, NoteE4, NoteA4 }, 1));  // A chord
-        REQUIRE_NOTHROW(chordsTest.setKey({ NoteF4, NoteD4, NoteF4, NoteA4 }, 1));  // D chord
-
-        // Make sure there was no output.
-        REQUIRE(note_output.empty());
-        REQUIRE(velocity_output.empty());
-
-        WHEN("xx"){
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteD4, NOTE_ON  }, 0)); // F Major ON
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteC4, NOTE_ON  }, 0)); // C Major ON
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteD4, NOTE_OFF }, 0)); // F Major OFF
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteD4, NOTE_ON  }, 0)); // F Major ON
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteD4, NOTE_OFF }, 0)); // F Major OFF
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteE4, NOTE_ON  }, 0)); // A Major ON
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteF4, NOTE_ON  }, 0)); // D Major OFF
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteC4, NOTE_OFF }, 0));  // C Major OFF
-
-            //REQUIRE(note_output.size() == 20);
-            //REQUIRE(velocity_output.size() == 20);
-
-            THEN("axax"){
-                int expectedOutput[][2] = {
-                    { NoteC4, NOTE_ON  },  // F Major ON  C4
-                    { NoteF4, NOTE_ON  },  //             C4 F4
-                    { NoteA4, NOTE_ON  },  //             C4 F4 A4
-                    { NoteC4, NOTE_ON  },  // C Major ON  C4 F4 A4 C4
-                    { NoteE4, NOTE_ON  },  //             C4 F4 A4 C4 E4
-                    { NoteG4, NOTE_ON  },  //             C4 F4 A4 C4 E4 G4
-                    { NoteC4, NOTE_OFF },  // F Major OFF F4 A4 E4 G4
-                    { NoteF4, NOTE_OFF },  //             A4 E4 G4
-                    { NoteA4, NOTE_OFF },  //             E4 G4
-                    { NoteC4, NOTE_ON  },  // F Major ON  E4 G4 C4
-                    { NoteF4, NOTE_ON  }, //             E4 G4 C4 F4
-                    { NoteA4, NOTE_ON  }, //             E4 G4 C4 F4 A4
-                    { NoteC4, NOTE_OFF }, // F Major OFF E4 G4 F4 A4
-                    { NoteF4, NOTE_OFF }, //             E4 G4 A4
-                    { NoteA4, NOTE_OFF }, //             E4 G4
-                    { NoteC4, NOTE_ON  }, // A Major ON  E4 G4 C4
-                    { NoteE4, NOTE_ON  }, //             E4 G4 C4 E4
-                    { NoteA4, NOTE_ON  }, //             E4 G4 C4 E4 A4
-                    { NoteD4, NOTE_ON  }, // D Major OFF E4 G4 C4 E4 A4 D4
-                    { NoteF4, NOTE_ON  }, //             E4 G4 C4 E4 A4 D4 F4
-                    { NoteA4, NOTE_ON  }, //             E4 G4 C4 E4 A4 D4 F4 A4
-                    { NoteC4, NOTE_OFF }, // C Major OFF E4 G4 E4 A4 D4 F4 A4
-                    { NoteE4, NOTE_OFF }, //             E4 G4 E4 A4 D4 F4 A4
-                    { NoteG4, NOTE_OFF }  //             E4 E4 A4 D4 F4 A4
-                };
-
-                for (int i = 0; i < note_output.size(); i++) {
-                    //REQUIRE(note_output[i][1] == expectedOutput[i][0]);
-                    //REQUIRE(velocity_output[i][1] == expectedOutput[i][1]);
-                }
-
-                //REQUIRE(note_output.size() == 24);
-                //REQUIRE(velocity_output.size() == 24);
-            }
-        }
-    }
-}
-
-SCENARIO("Shared notes in different order NoteMode::RETRIGGER") {
-    std::cout << "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n";
-    ext_main(nullptr);
-
-    min::test_wrapper<ChordsMax> an_instance;
-    ChordsMax &chordsTest = an_instance;
-
-    auto &note_output = *max::object_getoutput(chordsTest, 0);
-    auto &velocity_output = *max::object_getoutput(chordsTest, 1);
-
-    REQUIRE_NOTHROW(chordsTest.noteMode(ChordsMax::NoteMode::RETRIGGER, 1));
-    REQUIRE(chordsTest.getNoteMode() == ChordsMax::NoteMode::RETRIGGER);
-
-    GIVEN("An instance of our object") {
-        REQUIRE_NOTHROW(chordsTest.setKey({ NoteC4, NoteC4, NoteE4, NoteG4 }, 1)); // C Major
-        REQUIRE_NOTHROW(chordsTest.setKey({ NoteD4, NoteC4, NoteF4, NoteA4 }, 1)); // F Major
-        REQUIRE_NOTHROW(chordsTest.setKey({ NoteE4, NoteC4, NoteE4, NoteA4 }, 1)); // A Major
-
-        WHEN("press couple of keys") {    
-            // Play in opposite order
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteC4, NOTE_ON }, 0));  // C chord first
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteD4, NOTE_ON }, 0));  // F chord second
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteE4, NOTE_ON }, 0));  // A chord
-            REQUIRE_NOTHROW(chordsTest.listInput({ NoteE4, NOTE_OFF }, 0)); // Release A chord
-            
-            REQUIRE(!note_output.empty());
-
-            THEN("make sure the output is correct") {
-                REQUIRE(note_output.size() == 9);
-                REQUIRE(velocity_output.size() == 9);
-
-                REQUIRE( note_output[0][1] == NoteC4 ); // C Major ON
-                REQUIRE( note_output[1][1] == NoteE4 ); 
-                REQUIRE( note_output[2][1] == NoteG4 );
-                REQUIRE( note_output[3][1] == NoteC4 ); // F Major ON
-                REQUIRE( note_output[4][1] == NoteF4 );
-                REQUIRE( note_output[5][1] == NoteA4 );
-                REQUIRE( note_output[6][1] == NoteC4 ); // A Major ON
-                REQUIRE( note_output[7][1] == NoteE4 );
-                REQUIRE( note_output[8][1] == NoteA4 );
-                //{ NoteC4, NOTE_OFF}, // A Major OFF
-                //{ NoteE4, NOTE_OFF},
-                //{ NoteA4, NOTE_OFF}
-
-                REQUIRE( velocity_output[0][1] == NOTE_ON );
-                REQUIRE( velocity_output[1][1] == NOTE_ON );
-                REQUIRE( velocity_output[2][1] == NOTE_ON );
-                REQUIRE( velocity_output[3][1] == NOTE_ON );
-                REQUIRE( velocity_output[4][1] == NOTE_ON );
-                REQUIRE( velocity_output[5][1] == NOTE_ON );
-                REQUIRE( velocity_output[6][1] == NOTE_ON );
-                REQUIRE( velocity_output[7][1] == NOTE_ON );
-                REQUIRE( velocity_output[8][1] == NOTE_ON );
-                //{ NoteC4, NOTE_OFF}, // A Major OFF
-                //{ NoteE4, NOTE_OFF},
-                //{ NoteA4, NOTE_OFF}
-            }
-        }
-    }
-    std::cout << "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n";
 }
 
 SCENARIO("Shared notes in different order NoteMode::LEGATO") {
@@ -271,6 +131,9 @@ SCENARIO("Shared notes in different order NoteMode::LEGATO") {
             REQUIRE(note_output[3][1] == NoteD4); // D Minor ON
             REQUIRE(note_output[4][1] == NoteF4);
             REQUIRE(note_output[5][1] == NoteA4);
+            REQUIRE(note_output[6][1] == NoteD4); // F Major ON
+            REQUIRE(note_output[7][1] == NoteF4);
+            REQUIRE(note_output[8][1] == NoteA4);
 
             REQUIRE(velocity_output[0][1] == NOTE_ON); // C Major ON
             REQUIRE(velocity_output[1][1] == NOTE_ON);
@@ -278,12 +141,15 @@ SCENARIO("Shared notes in different order NoteMode::LEGATO") {
             REQUIRE(velocity_output[3][1] == NOTE_ON); // D Minor ON
             REQUIRE(velocity_output[4][1] == NOTE_ON);
             REQUIRE(velocity_output[5][1] == NOTE_ON);
+            REQUIRE(velocity_output[6][1] == NOTE_OFF); // F Major ON
+            REQUIRE(velocity_output[7][1] == NOTE_OFF);
+            REQUIRE(velocity_output[8][1] == NOTE_OFF);
 
-            //REQUIRE(note_output.size() == 6);
-            //REQUIRE(velocity_output.size() == 6);
+            REQUIRE(note_output.size() == 9);
+            REQUIRE(velocity_output.size() == 9);
         }
     }
-}
+};
 
 SCENARIO("enable/disable") { // NOLINT
     ext_main(nullptr);
@@ -317,7 +183,7 @@ SCENARIO("enable/disable") { // NOLINT
         REQUIRE(velocity_output.size() == 2);
         REQUIRE(note_output.size() == 2);
     }
-}
+};
 
 SCENARIO("set key notes") { // NOLINT
     ext_main(nullptr);
@@ -347,4 +213,4 @@ SCENARIO("set key notes") { // NOLINT
         REQUIRE(note_output.size() == 8);
         REQUIRE(velocity_output.size() == 8);
     }
-}
+};
